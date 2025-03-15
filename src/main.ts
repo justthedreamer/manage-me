4c702fd4725dce949dacf96ef62512d06d858e2f
Piotr Zapala <piotr.zapala.dev@gmail.com>
2025-03-15 18:59:22 +0100

    setup pinia store.
    Signed-off-by: Piotr Zapala <piotr.zapala.dev@gmail.com>

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,16 @@ import {createApp} from 'vue'
 import './style.css'
 import App from './App.vue'
 import router from "./routing/Router";
+import {createPinia} from "pinia";
 
+const pinia = createPinia()
 const app = createApp(App)
 
 app.config.errorHandler = (err, vm, info) => {
     console.error("Global error handler: ", err, info)
 }
 
+app.use(pinia)
 app.use(router)
 
 app.mount('#app')
