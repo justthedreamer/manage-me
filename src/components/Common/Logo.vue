e7972618d83bdc1eeb0b858d86c3d8aa8e6c8f25
Piotr Zapala <piotr.zapala.dev@gmail.com>
2025-03-22 15:43:16 +0100

    feat: implement logo component.
    Signed-off-by: Piotr Zapala <piotr.zapala.dev@gmail.com>

---
+++ src/components/Common/Logo.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import type {PropType} from "vue";
+
+export type LogoThemeType = "text-dark" | "text-light";
+
+defineProps({
+  theme: {
+    type: String as PropType<LogoThemeType>,
+    required: true,
+  }
+})
+
+</script>
+
+<template>
+  <div class="logo">
+    <h1
+        :class="theme"
+        class="text-uppercase">
+      manage<span class="text-primary fw-bold">!</span>me
+    </h1>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
