37cb73af48f36eb3afe7064d4eb07ba0c0a43e51
Piotr Zapala <piotr.zapala.dev@gmail.com>
2025-03-22 15:45:37 +0100

    refactor: update main applicaion template.
    Signed-off-by: Piotr Zapala <piotr.zapala.dev@gmail.com>

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,14 @@
 <script setup lang="ts">
+import TopNavbar from "./components/Navigation/TopNavbar.vue";
+import UiMessages from "./components/Common/UIMessages.vue";
+import MaxWidthLimiter from "./components/Common/MaxWidthLimiter.vue";
 </script>
 <template>
-  <RouterView/>
+  <top-navbar/>
+  <ui-messages/>
+  <max-width-limiter>
+    <main class="m-4">
+      <router-view/>
+    </main>
+  </max-width-limiter>
 </template>
