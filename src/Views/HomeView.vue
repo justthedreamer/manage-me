0f838e959d924b21e67acf251ccb5ffb3b0087dc
Piotr Zapala <piotr.zapala.dev@gmail.com>
2025-03-22 15:44:52 +0100

    refactor: clean home view template.
    Signed-off-by: Piotr Zapala <piotr.zapala.dev@gmail.com>

--- src/Views/HomeView.vue
+++ src/Views/HomeView.vue
@@ -1,6 +1,13 @@
 <script setup lang="ts">
-import TopNavigation from "../components/TopNavigation.vue";
+import Logo from "../components/Common/Logo.vue";
 </script>
+
 <template>
-  <TopNavigation/>
+  <div class="d-flex flex-column w-100 align-items-center mt-5">
+    <logo :theme="'text-dark'"/>
+    <p class="text-center">Is an open-source project management tool crafted in vue.js framework.</p>
+    <a href="https://github.com/justthedreamer/manage-me"
+       class="text-decoration-none text-dark">
+      <i class="bi bi-github fs-4"></i></a>
+  </div>
 </template>
