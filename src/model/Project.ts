6fe970e3c6dbdc6bd38c85d86d085831d2c744af
Piotr Zapala <piotr.zapala.dev@gmail.com>
2025-03-22 15:24:12 +0100

    use type instead of clas in Project model declaration and implement ProjectStory.
    Signed-off-by: Piotr Zapala <piotr.zapala.dev@gmail.com>

--- src/model/Project.ts
+++ src/model/Project.ts
@@ -1,14 +1,13 @@
+import type {ProjectStory} from "./ProjectStory.ts";
+import type {UUIDTypes} from "uuid";
+
 /**
  * Represents project.
  */
-export class Project {
-    id: number;
-    name: string;
-    description: string;
-
-    constructor(id: number, name: string, description: string) {
-        this.id = id;
-        this.name = name;
-        this.description = description;
-    }
+export type Project = {
+    id: UUIDTypes,
+    name: string,
+    description: string,
+    stories: ProjectStory[]
 }
+
