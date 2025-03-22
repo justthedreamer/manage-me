f2d2c786545617bb06e84b7799a10cd63354a552
Piotr Zapala <piotr.zapala.dev@gmail.com>
2025-03-22 15:43:04 +0100

    feat: implement full screen wrapper.
    Signed-off-by: Piotr Zapala <piotr.zapala.dev@gmail.com>

---
+++ src/components/Common/FullScreenWrapper.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div
+      class="position-fixed vw-100 vh-100 top-0 start-0 d-flex justify-content-center align-items-center z-3 bg-blur overflow-y-scroll scroller">
+    <slot></slot>
+  </div>
+</template>
