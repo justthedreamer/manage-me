92bc3a1a5d4bf1c4b7f757ea03524d0aa38a1418
justthedreamer <piotr.zapala.dev@gmail.com>
2025-03-29 16:07:41 +0100

    feat: add new user mocks.
    Signed-off-by justthedreamer <piotr.zapala.dev@gmail.com>

--- src/mocks/UserMocks.ts
+++ src/mocks/UserMocks.ts
@@ -1,10 +1,31 @@
 import {newGuid} from "../helpers/GuidHelper.ts";
+import { UserRole } from "../model/enums/UserRole.ts";
 import type {User} from "../model/User.ts";
 import {projectMock} from "./ProjectMocks.ts";
 
-export const registeredUserMock: User = {
+
+export const adminUserMock: User= {
     id: newGuid(),
     name: "John",
-    surname: "Doe",
+    surname: "Admin",
+    role: UserRole.Admin,
+    attachedProjectId: projectMock.id
+}
+
+export const devopsUserMock : User = {
+    id: newGuid(),
+    name: "John",
+    surname: "Devops",
+    role: UserRole.Devops,
     attachedProjectId: projectMock.id,
 }
+
+export const developerUserMock: User = {
+    id: newGuid(),
+    name: "John",
+    surname: "Developer",
+    role : UserRole.Developer,
+    attachedProjectId: projectMock.id,
+}
+
+export const usersMock : User[] = [adminUserMock,devopsUserMock,developerUserMock]
