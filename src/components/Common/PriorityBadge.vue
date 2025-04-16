0742c936923051aa746ff6b7446e2108fe3aeb15
Piotr Zapala <piotr.zapala.dev@gmail.com>
2025-04-16 12:12:24 +0200

    feaT: create priority badge component.
    Signed-off-by: Piotr Zapala <piotr.zapala.dev@gmail.com>

---
+++ src/components/Common/PriorityBadge.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+
+import {computed, type PropType} from "vue";
+import {Priority} from "../../model/enums/Priority.ts";
+import {assertNever} from "../../helpers/SwitchHelper.ts";
+
+const props = defineProps({
+  priority: {
+    type: String as PropType<Priority>,
+    required: true
+  }
+})
+
+const utilities = computed(() => {
+  switch (props.priority) {
+    case Priority.LOW: {
+      return {
+        badge: "badge text-bg-success",
+      };
+    }
+    case Priority.MEDIUM: {
+      return {
+        badge: "badge text-bg-warning",
+      };
+    }
+    case Priority.HIGH: {
+      return {
+        badge: "badge text-bg-danger",
+      };
+    }
+    default:
+      assertNever(props.priority);
+  }
+})
+
+</script>
+
+<template>
+  <span class="badge align-self-center" :class="utilities.badge">{{ priority }}</span>
+</template>
+
+<style scoped>
+
+</style>
