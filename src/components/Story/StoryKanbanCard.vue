dd04a604d8428fc02bd58257da69f571844c7d6d
Piotr Zapala <piotr.zapala.dev@gmail.com>
2025-04-16 12:23:58 +0200

    feat: create story kanban card.
    Signed-off-by: Piotr Zapala <piotr.zapala.dev@gmail.com>

---
+++ src/components/Story/StoryKanbanCard.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+import type {PropType} from "vue";
+import type {Story} from "../../model/Story.ts";
+import PriorityBadge from "../Common/PriorityBadge.vue";
+import {formatDateToEuropean} from "../../helpers/DateHelper.ts";
+import {useRouter} from "vue-router";
+import {Routes} from "../../routing/Routes.ts";
+
+const router = useRouter()
+
+const props = defineProps({
+  story: {
+    type: Object as PropType<Story>,
+    required: true,
+  }
+})
+
+function goDetails() {
+  router.push({name: Routes.PROJECT_STORY_ROUTE_RECORD.name, params: {id: props.story!.id.toString()}})
+}
+</script>
+
+<template>
+  <div class="border rounded bg-white shadow-sm w-100 p-2 mt-1 mb-1 p-2">
+    <header class="d-flex justify-content-between align-items-center text-dark-emphasis gap-1">
+      <p class="mb-0">{{ story.name }}</p>
+      <priority-badge :priority="story.priority"/>
+    </header>
+
+    <hr>
+
+    <footer class="d-flex justify-content-between align-items-center text-secondary">
+      <div>
+        <span>{{ formatDateToEuropean(story.createdAt) }}</span>
+      </div>
+      <span class="bi bi-arrow-bar-right" style="cursor: pointer" @click="goDetails" title="Go to details"></span>
+    </footer>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
