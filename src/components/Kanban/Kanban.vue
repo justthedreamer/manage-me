d3c591de0e5595d8ba0cc6d6ee4ee9db80148741
Piotr Zapala <piotr.zapala.dev@gmail.com>
2025-04-16 12:13:41 +0200

    feat: create kanban component.
    Signed-off-by: Piotr Zapala <piotr.zapala.dev@gmail.com>

---
+++ src/components/Kanban/Kanban.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="kanban border border-1 rounded p-2 bg-highlight d-flex gap-1">
+    <slot></slot>
+  </div>
+</template>
+
+<style scoped>
+.kanban {
+  min-height: 600px;
+}
+
+@media (max-width: 720px) {
+  .kanban {
+    flex-direction: column;
+  }
+}
+</style>
---
+++ src/components/Kanban/KanbanCol.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+defineProps({
+  title: {
+    type: String,
+    required: true
+  },
+})
+</script>
+
+<template>
+  <div class="overflow-y-scroll scroller border border-1 rounded w-100 p-1">
+    <span>{{ title }}</span>
+    <hr>
+    <slot></slot>
+  </div>
+</template>
+
+<style scoped>
+div {
+  max-height: 600px;
+}
+</style>
