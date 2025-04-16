13fcabbd7fcf85e05f0c1b13c0b1c438e7cf6270
Piotr Zapala <piotr.zapala.dev@gmail.com>
2025-04-16 12:10:00 +0200

    feat: create user role badge component.
    Signed-off-by: Piotr Zapala <piotr.zapala.dev@gmail.com>

---
+++ src/components/Badges/UserRoleBadge.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import type {PropType} from "vue";
+import {UserRole} from "../../model/enums/UserRole.ts";
+
+defineProps({
+  role: {
+    type: Number as PropType<UserRole>
+  }
+})
+</script>
+
+<template>
+  <span v-if="role === UserRole.DEVELOPER">
+    <span class="bi bi-keyboard"></span>
+    <span class="text-uppercase ms-1">dev</span>
+  </span>
+  <span v-if="role === UserRole.DEVOPS">
+    <span class="bi bi-gear"></span>
+    <span class="text-uppercase ms-1">devops</span>
+  </span>
+  <span v-if="role === UserRole.ADMIN">
+    <span class="bi bi-person-up"></span>
+    <span class="text-uppercase ms-1">admin</span>
+  </span>
+</template>
+
+<style scoped>
+
+</style>
