edb8bea1f9990618f8077b4b491318f174e33449
Piotr Zapala <piotr.zapala.dev@gmail.com>
2025-04-16 12:09:41 +0200

    feat: create time hours badge component.
    Signed-off-by: Piotr Zapala <piotr.zapala.dev@gmail.com>

---
+++ src/components/Badges/TimeHoursBadge.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+defineProps({
+  hours: {
+    type: Number,
+    required: true,
+  }
+})
+</script>
+
+<template>
+  <span class="badge bg-success">{{ hours }}H</span>
+</template>
+
+<style scoped>
+
+</style>
