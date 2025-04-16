f75c15f68e84ee610048d41222fa28b9335e4ce4
Piotr Zapala <piotr.zapala.dev@gmail.com>
2025-04-16 12:46:15 +0200

    refactor: cleanup home view.
    Signed-off-by: Piotr Zapala <piotr.zapala.dev@gmail.com>

--- src/Views/HomeView.vue
+++ src/Views/HomeView.vue
@@ -4,10 +4,9 @@ import Logo from "../components/Common/Logo.vue";
 
 <template>
   <div class="d-flex flex-column w-100 align-items-center mt-5">
-    <logo :theme="'text-dark'"/>
+    <logo :theme="'text-dark'" />
     <p class="text-center">Is an open-source project management tool crafted in vue.js framework.</p>
-    <a href="https://github.com/justthedreamer/manage-me"
-       class="text-decoration-none text-dark">
+    <a href="https://github.com/justthedreamer/manage-me" class="text-decoration-none text-dark">
       <i class="bi bi-github fs-4"></i></a>
   </div>
 </template>
