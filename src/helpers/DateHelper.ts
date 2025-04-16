060ad1616080042ee4e8968cfb4dbecc8f6f9a73
Piotr Zapala <piotr.zapala.dev@gmail.com>
2025-04-16 12:08:42 +0200

    refactor: change date formating function to display date, sperated by dots.
    Signed-off-by: Piotr Zapala <piotr.zapala.dev@gmail.com>

--- src/helpers/DateHelper.ts
+++ src/helpers/DateHelper.ts
@@ -1,4 +1,4 @@
 export function formatDateToEuropean(date: Date): string {
     const d = new Date(date);
-    return `${d.getDay()}/${d.getMonth() + 1}/${d.getFullYear()} ${d.getHours()}:${d.getMinutes()}`;
+    return `${d.getDay()}.${d.getMonth() + 1}.${d.getFullYear()} ${d.getHours()}:${d.getMinutes()}`;
 }
