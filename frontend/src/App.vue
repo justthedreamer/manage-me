aabd0d407c5fed5af4762b160ded67bbc5c18b11
Piotr Zapala <piotr.zapala.dev@gmail.com>
2025-06-09 15:50:45 +0200

    restore user theme on application load.
    Signed-off-by: Piotr Zapala <piotr.zapala.dev@gmail.com>

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,16 +2,19 @@
 import TopNavbar from "./components/navigation/TopNavbar.vue";
 import MaxWidthLimiter from "./components/utils/MaxWidthLimiter.vue";
 import Loader from "./components/common/Loader.vue";
-import UserAside from "./components/navigation/UserAside.vue";
 import UIMessages from "./components/common/UIMessages.vue";
 import {onMounted} from "vue";
 import userService from "./services/UserService.ts";
+import UserAside from "./components/navigation/UserAside.vue";
+import themeService from "./services/ThemeService.ts";
 
 onMounted(async () => {
   await userService.restoreUserStateAsync()
+  themeService.restoreTheme()
 })
 
 </script>
+
 <template>
   <top-navbar/>
   <user-aside/>
